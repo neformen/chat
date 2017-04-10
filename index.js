f134d127719c8462b5ba75911a937c68970b6b2b
Vasyl_Motsyak <Vasyl_Motsyak@epam.com>
2017-04-10 15:15:34 +0300

    added html template

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var port = process.env.PORT || 5000;
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(port, function(){
